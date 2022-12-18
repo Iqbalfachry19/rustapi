a2f107cc18acce12501af5d05a0196c2f8b0f146
iqbalfachry19 <ifachry@rocketmail.com>
2022-12-18 23:55:27 +0700

    hi

--- api/index.rs
+++ api/index.rs
@@ -1,24 +1,18 @@
-use http::{StatusCode, uri::PathAndQuery};
+use http::{StatusCode};
 use vercel_lambda::{lambda, error::VercelError, IntoResponse, Request, Response};
 use std::error::Error;
 
-fn handler(request: Request) -> Result<impl IntoResponse, VercelError> {
-    let path_and_query = request.uri().path_and_query().ok_or("Invalid URI")?;
-    let path_segments = path_and_query.path().split('/').collect::<Vec<_>>();
-    let query_string = path_and_query.query().unwrap_or("");
+fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
+	let response = Response::builder()
+		.status(StatusCode::OK)
+		.header("Content-Type", "text/plain")
+		.body("Hello World")
+		.expect("Internal Server Error");
 
-    if path_segments.len() != 2 {
-        return Err("Invalid path".into());
-    }
+		Ok(response)
+}
 
-    let id = path_segments[1];
-    let query_params = query_string.split('&').collect::<Vec<_>>();
-
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "text/plain")
-        .body(format!("id: {}, query_params: {:?}", id, query_params))
-        .expect("Internal Server Error");
-
-    Ok(response)
+// Start the runtime with the handler
+fn main() -> Result<(), Box<dyn Error>> {
+	Ok(lambda!(handler))
 }
